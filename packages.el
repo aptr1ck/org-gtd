ef3690cee064c8f59ebfba94c9737fbb5bad5482
Patrick McDermott <patrick@tuitechnology.co.nz>
2021-07-16 16:23:40 +1200

    Added code for adding inactive timestamps.
    Also has function for automatically adding created (inactive) timestamps for new headings.

--- packages.el
+++ packages.el
@@ -1065,6 +1065,28 @@ Skip project and sub-project tasks, habits, and loose non-project tasks."
 
   (setq org-list-allow-alphabetical t)
 
+  ;; create timestamps when headings are created
+  (defvar bh/insert-inactive-timestamp t)
+
+  (defun bh/toggle-insert-inactive-timestamp ()
+    (interactive)
+    (setq bh/insert-inactive-timestamp (not bh/insert-inactive-timestamp))
+    (message "Heading timestamps are %s" (if bh/insert-inactive-timestamp "ON" "OFF")))
+
+  (defun bh/insert-inactive-timestamp ()
+    (interactive)
+    (org-insert-time-stamp nil t t nil nil nil))
+
+  (defun bh/insert-heading-inactive-timestamp ()
+    (save-excursion
+      (when bh/insert-inactive-timestamp
+        (org-return)
+        (org-cycle)
+        (bh/insert-inactive-timestamp))))
+
+  (add-hook 'org-insert-heading-hook 'bh/insert-heading-inactive-timestamp 'append)
+  ;; end timestamps
+
   ;; ;; Explicitly load required exporters
   ;; (require 'ox-html)
   ;; (require 'ox-latex)
